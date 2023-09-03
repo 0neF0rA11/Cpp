5db7d63249d3d6ca7537b7dcf5c369e0ac68c682
Александр Олейников <131586357+0neF0rA11@users.noreply.github.com>
2023-09-03 20:08:48 +0300

    Add files via upload

---
+++ Functions/FunctionWithArguments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+/*
+void factorial(int number)
+{
+	int amount = 1;
+	
+	for (int i = 1; i < number+1; ++i)
+		amount *= i;
+	cout << amount;
+}
+
+int main()
+{
+	factorial(5);
+}
+*/
